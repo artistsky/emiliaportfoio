756512ac148b369703afae6ef683e96133a7b1cf
Gabe Troyan <https://github.com/artistsky/artistsky.github.io.git>
2018-12-04 17:46:42 -0500

    No prefix

--- config/site.js
+++ config/site.js
@@ -1,9 +1,9 @@
 module.exports = {
-  pathPrefix: 'gatsby-starter-portfolio-emilia', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
   siteTitle: 'Emilia', // Navigation and Site Title
   siteTitleAlt: 'Emilia - Gatsby Starter Portfolio', // Alternative Site title for SEO
-  siteUrl: 'https://portfolio-emilia.netlify.com', // Domain of your site. No trailing slash!
+  siteUrl: 'https://emilia.gabe.ws', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo-1024.png', // Used for SEO and manifest
   siteDescription: 'Dark One-Page portfolio with cards & detailed project views',
