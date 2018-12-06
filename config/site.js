e7dd2b778b8111934aaf44b484f4af33fdea7a08
artistsky <44006932+artistsky@users.noreply.github.com>
2018-12-05 22:44:49 -0500

    Update site.js

--- config/site.js
+++ config/site.js
@@ -1,9 +1,9 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
-  siteTitle: 'Emilia', // Navigation and Site Title
-  siteTitleAlt: 'Emilia - Gatsby Starter Portfolio', // Alternative Site title for SEO
-  siteUrl: 'https://emilia.gabe.ws', // Domain of your site. No trailing slash!
+  siteTitle: 'Sarah', // Navigation and Site Title
+  siteTitleAlt: 'Sarah - Starter Portfolio', // Alternative Site title for SEO
+  siteUrl: 'https://website.sarahbellefeuille.com/', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo-1024.png', // Used for SEO and manifest
   siteDescription: 'Dark One-Page portfolio with cards & detailed project views',
@@ -25,16 +25,16 @@ module.exports = {
 
   // Your information
   avatar: '/logos/social.png',
-  name: 'LekoArts',
-  location: 'Germany',
+  name: 'Sarah Bellefeuille',
+  location: 'United States',
   socialMedia: [
     {
       url: 'https://www.facebook.com/lekoarts.de',
-      name: 'Facebook',
+      name: 'LinkedIn',
     },
     {
       url: 'https://www.instagram.com/lekoarts.de',
-      name: 'Instagram',
+      name: 'Vimeo',
     },
   ],
 };
